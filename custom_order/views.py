8cf29aab815ef7c11e1376c521164a6d133f6058
Gilleece <seangilleece@gmail.com>
2021-05-14 12:56:19 +0000

    Added products custom_order view and template

--- custom_order/views.py
+++ custom_order/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def custom_print(request):
+    """ A view to return the custom order/print page """
+
+    return render(request, 'custom_order/custom_order.html')
