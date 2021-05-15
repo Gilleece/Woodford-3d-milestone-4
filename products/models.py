4f4a39f2af84d2cdf30e65cbedd8cb88bee1a357
Gilleece <seangilleece@gmail.com>
2021-05-15 14:52:35 +0000

    Added color to product model and color selector box to product template

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_colors = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
