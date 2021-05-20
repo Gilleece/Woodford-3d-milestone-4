a96bfdb7d5420ea91c1704a1b92fa48ab46fec43
Gilleece <seangilleece@gmail.com>
2021-05-20 14:17:27 +0000

    Added blog url, views and all_post.html template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def all_posts(request):
+    """ A view to return the main blog page """
+    return render(request, 'blog/all_posts.html')
