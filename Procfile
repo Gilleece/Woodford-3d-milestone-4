6cfc97fe0f6df3acb4e4cf7d3486b91b05856d48
Gilleece <seangilleece@gmail.com>
2021-05-21 21:05:00 +0000

    second stage of deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn woodford_3d.wsgi:application
--- woodford_3d/settings.py
+++ woodford_3d/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^6l2g-e7)+dctd!g%2x$&p^rlimna(n8i6-s)zkh*&)g*&zv@&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['woodford3d.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,12 +124,17 @@ WSGI_APPLICATION = 'woodford_3d.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -203,6 +208,6 @@ EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
 
 
 # Mail Chimp (For mailing list)
-MAILCHIMP_API_KEY=''
+MAILCHIMP_API_KEY = os.environ.get('MAILCHIMP_API_KEY')
 MAILCHIMP_DATA_CENTER = 'us6'
 MAILCHIMP_EMAIL_LIST_ID = 'de42c61472'
