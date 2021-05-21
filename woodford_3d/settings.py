624a8467e4716779d44a68b098dba477bddf2a1c
Gilleece <seangilleece@gmail.com>
2021-05-21 21:28:27 +0000

    Settings.py change to enable deployment

--- woodford_3d/settings.py
+++ woodford_3d/settings.py
@@ -126,13 +126,13 @@ WSGI_APPLICATION = 'woodford_3d.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
