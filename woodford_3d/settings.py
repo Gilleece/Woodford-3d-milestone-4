10d8ad4b3e8fd94b3e7702f82803f73869613eb0
Gilleece <seangilleece@gmail.com>
2021-05-22 19:32:19 +0000

    Set up email through sendgrid

--- woodford_3d/settings.py
+++ woodford_3d/settings.py
@@ -109,8 +109,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -216,9 +214,8 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'sales@woodford3D.com'
 
-# About app contact form
+# Email settings
 CONTACT_EMAIL = 'contact@woodford3D.com'
 ADMIN_EMAIL = ['contact@woodford3D.com']
 
@@ -235,3 +232,14 @@ EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
 MAILCHIMP_API_KEY = os.environ.get('MAILCHIMP_API_KEY')
 MAILCHIMP_DATA_CENTER = 'us6'
 MAILCHIMP_EMAIL_LIST_ID = 'de42c61472'
+
+if 'DEVELOPMENT' in os.environ:
+    DEFAULT_FROM_EMAIL = 'contact@woodford3D.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = 'apikey'
+    EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+    DEFAULT_FROM_EMAIL = 'contact@woodford3D.com'
